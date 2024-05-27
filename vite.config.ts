a9571be33d3d6cfb7c15d0550bb958e51356a2ca
coji <coji@techtalk.jp>
2024-05-27 13:57:49 +0900

    chore: Update vite.config.ts and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import mdx from '@mdx-js/rollup'
 import { vitePlugin as remix } from '@remix-run/dev'
+import { installGlobals } from '@remix-run/node'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+installGlobals({ nativeFetch: true })
+
 export default defineConfig({
   plugins: [
-    mdx(),
     remix({
       future: {
         v3_fetcherPersist: true,
