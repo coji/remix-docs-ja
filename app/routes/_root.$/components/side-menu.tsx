a69e5da6f16ea9a5f6c5d17f60666b4ceb982844
coji <coji@techtalk.jp>
2024-05-27 20:08:08 +0900

    chore: Update side menu styles in _root route component

--- app/routes/_root.$/components/side-menu.tsx
+++ app/routes/_root.$/components/side-menu.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from '@remix-run/react'
 import { twc } from 'react-twc'
 
-export const SideMenu = twc.div`mx-4 flex flex-col gap-4 text-sm text-muted-foreground `
+export const SideMenu = twc.div`mx-4 flex flex-col gap-4 text-sm text-muted-foreground break-words`
 export const SideMenuCategory = twc.div`mx-2 group`
 export const SideMenuCategoryTitle = twc.div`my-1 px-2 py-1`
 export const SideMenuItem = twc.div`ml-4 leading-6`
