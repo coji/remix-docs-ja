0649aa6891fe7a2f0a804866b286863b3b1875cd
coji <coji@techtalk.jp>
2024-05-29 08:20:09 +0900

    refactor: Update redirect URL construction in resources route

--- app/routes/resources.ts
+++ app/routes/resources.ts
@@ -1,6 +1,6 @@
 import { redirect, type LoaderFunctionArgs } from '@remix-run/node'
 
 export const loader = ({ request }: LoaderFunctionArgs) => {
-  const search = request.url.search
-  return redirect(`https://remix.run/resources?${search}`)
+  const url = new URL(request.url)
+  return redirect(`https://remix.run/resources?${url.searchParams.toString()}`)
 }
