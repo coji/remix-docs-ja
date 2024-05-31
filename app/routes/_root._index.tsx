7df74f6bb86d8c757015413b9fe7aadbb8a00234
coji <coji@techtalk.jp>
2024-05-31 23:06:39 +0900

    refactor: Update markdown server and styles

--- app/routes/_root._index.tsx
+++ app/routes/_root._index.tsx
@@ -1,4 +1,10 @@
+import type { LinksFunction } from '@remix-run/node'
+import markdownStyles from '~/styles/md.css?url'
 import Page, { loader } from './_root.$/route'
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: markdownStyles },
+]
 
 export { loader }
 export default Page
