94bfebcde27e904a05a98d6e7ec319955459c269
coji <coji@techtalk.jp>
2024-06-01 22:59:54 +0900

    refactor: Add cheerio dependency and create table of contents from headings

--- app/routes/_root.$/route.tsx
+++ app/routes/_root.$/route.tsx
@@ -1,5 +1,6 @@
 import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node'
 import { useLoaderData, useRouteError } from '@remix-run/react'
+import { cn } from '~/libs/utils'
 import { getDoc } from '~/services/document.server'
 import markdownStyles from '~/styles/md.css?url'
 
@@ -24,11 +25,26 @@ export default function Docs() {
   const { doc } = useLoaderData<typeof loader>()
 
   return (
-    <div
-      className="md-prose prose overflow-x-hidden px-4 py-8 dark:prose-invert md:py-2"
-      // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
-      dangerouslySetInnerHTML={{ __html: doc.html }}
-    />
+    <div className="relative grid grid-cols-1 lg:grid-cols-[1fr_20rem]">
+      <div
+        className="md-prose prose overflow-x-hidden px-4 py-8 dark:prose-invert md:py-2"
+        // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+        dangerouslySetInnerHTML={{ __html: doc.html }}
+      />
+      <div>
+        <ul className="sticky top-20 hidden flex-col gap-2 px-8 lg:flex">
+          <div>目次</div>
+          {doc.headings.map((heading) => (
+            <li
+              key={heading.slug}
+              className={cn(heading.headingLevel === 'h3' && 'ml-4 text-sm')}
+            >
+              <a href={`#${heading.slug}`}>{heading.html}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- app/services/document.server.ts
+++ app/services/document.server.ts
@@ -1,4 +1,5 @@
 import { remember } from '@epic-web/remember'
+import { load as $ } from 'cheerio'
 import fs from 'node:fs/promises'
 import path from 'node:path'
 import { processMarkdown } from '~/services/md.server'
@@ -8,6 +9,20 @@ export const getDoc = async (file: string) => {
     const filepath = path.join('./docs', `${file}.md`)
     const content = await fs.readFile(filepath, 'utf-8')
     const doc = await processMarkdown(content)
-    return doc
+    const headings = createTableOfContentsFromHeadings(doc.html)
+    return { ...doc, headings }
   })
+}
+
+// create table of contents from h2 and h3 headings
+const createTableOfContentsFromHeadings = (html: string) => {
+  const $headings = $(html)('h2,h3')
+
+  const headings = $headings.toArray().map((heading) => ({
+    headingLevel: heading.name,
+    html: $(heading)('a').remove().end().children().html(),
+    slug: heading.attributes.find((attr) => attr.name === 'id')?.value,
+  }))
+
+  return headings
 }
