ee6c28c12d3b96aabfa23109f10780bba8600dc2
coji <coji@techtalk.jp>
2024-06-15 15:55:12 +0900

    fix: healthcheck undici memory leak

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,19 +1,5 @@
 import type { LoaderFunction } from '@remix-run/node'
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const host =
-    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
-
-  try {
-    const url = new URL('/', `http://${host}`)
-    await Promise.all([
-      fetch(url.toString(), { method: 'HEAD' }).then((r) => {
-        if (!r.ok) return Promise.reject(r)
-      }),
-    ])
-    return new Response('OK')
-  } catch (error: unknown) {
-    console.log('healthcheck ❌', { error })
-    return new Response('ERROR', { status: 500 })
-  }
+export const loader: LoaderFunction = ({ request }) => {
+  return new Response('OK')
 }
