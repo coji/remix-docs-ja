ca90ffd80363ce02459a399e332715f6c5f07d74
coji <coji@techtalk.jp>
2024-09-21 23:28:46 +0900

    Refactor getDomain function to use X-Forwarded-Host header

--- app/features/product/get-product.ts
+++ app/features/product/get-product.ts
@@ -4,8 +4,9 @@ export const getProduct = (request: Request) => {
   // リクエストURLのドメイン名から、都市を判定する
   // 例: https://remix-docs-ja.example.com -> product = 'remix-docs-ja'
   // 例: https://react-router-docs-ja.example.com -> product = 'react-router-docs-ja'
-  const url = new URL(request.url)
-  const host = url.host.split('.')[0]
+  const origin =
+    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+  const host = origin?.split('.')[0]
   const product = products.find((prd) => prd.id === host) ?? products[0]
   return { product }
 }
