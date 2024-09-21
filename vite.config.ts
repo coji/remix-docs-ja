93a9a7f38921d3f3a6b39b3bcabcac95daa12c43
coji <coji@techtalk.jp>
2024-09-21 21:53:00 +0900

    Update biome.json and vite.config.ts files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import mdx from '@mdx-js/rollup'
 import { vitePlugin as remix } from '@remix-run/dev'
-import { installGlobals } from '@remix-run/node'
 import remarkFrontmatter from 'remark-frontmatter'
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-installGlobals({ nativeFetch: true })
 
 declare module '@remix-run/server-runtime' {
   interface Future {
