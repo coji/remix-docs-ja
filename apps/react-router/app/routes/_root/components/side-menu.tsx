e089f776969fd2076fd3b327d241e6a353d34fe6
coji <coji@techtalk.jp>
2025-04-12 13:36:41 +0900

    feat: enhance side menu with ChevronRight icon and improve styling

--- apps/react-router/app/routes/_root/components/side-menu.tsx
+++ apps/react-router/app/routes/_root/components/side-menu.tsx
@@ -1,3 +1,4 @@
+import { ChevronRight } from 'lucide-react'
 import { NavLink } from 'react-router'
 import { twc } from 'react-twc'
 import type { MenuDoc } from '~/services/menu.server'
@@ -7,52 +8,65 @@ interface SideMenuProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 export const SideMenu = ({ menu }: SideMenuProps) => {
   return (
-    <div className="bg-background text-muted-foreground mx-2 hidden flex-col gap-4 overflow-auto pb-8 text-sm break-words whitespace-normal md:flex">
+    <SideMenuContainer>
       {menu.map((category) => {
         return (
           <SideMenuCategory key={category.slug}>
-            <SideMenuCategoryTitle className="group-has-[.active]:bg-muted rounded-md group-has-[.active]:font-bold">
+            <SideMenuCategoryTitle>
               {category.attrs.title}
             </SideMenuCategoryTitle>
 
-            {category.children.map((menuItem) => {
-              return (
-                <SideMenuItem key={menuItem.slug}>
-                  {menuItem.children.length > 0 ? (
-                    <div>{menuItem.attrs.title}</div>
-                  ) : (
-                    <SideMenuNavLink
-                      to={`${menuItem.slug}`}
-                      prefetch="viewport"
-                    >
-                      {menuItem.attrs.title}
-                    </SideMenuNavLink>
-                  )}
+            <SideMenuContent>
+              {category.children.map((menuItem) => {
+                return (
+                  <SideMenuItem key={menuItem.slug}>
+                    {menuItem.children.length > 0 ? (
+                      <div>
+                        <span>{menuItem.attrs.title}</span>
+                        <ChevronRight className="inline size-4" />
+                      </div>
+                    ) : (
+                      <SideMenuNavLink
+                        to={`${menuItem.slug}`}
+                        prefetch="viewport"
+                      >
+                        {menuItem.attrs.title}
+                      </SideMenuNavLink>
+                    )}
 
-                  {menuItem.children.map((subMenuItem) => {
-                    return (
-                      <SideMenuItem key={subMenuItem.slug}>
-                        <SideMenuNavLink
-                          to={`${subMenuItem.slug}`}
-                          prefetch="viewport"
-                        >
-                          {subMenuItem.attrs.title}
-                        </SideMenuNavLink>
-                      </SideMenuItem>
-                    )
-                  })}
-                </SideMenuItem>
-              )
-            })}
+                    {menuItem.children.length > 0 && (
+                      <SideMenuSubContent>
+                        {menuItem.children.map((subMenuItem) => {
+                          return (
+                            <SideMenuItem key={subMenuItem.slug}>
+                              <SideMenuNavLink
+                                to={`${subMenuItem.slug}`}
+                                prefetch="viewport"
+                              >
+                                {subMenuItem.attrs.title}
+                              </SideMenuNavLink>
+                            </SideMenuItem>
+                          )
+                        })}
+                      </SideMenuSubContent>
+                    )}
+                  </SideMenuItem>
+                )
+              })}
+            </SideMenuContent>
           </SideMenuCategory>
         )
       })}
-    </div>
+    </SideMenuContainer>
   )
 }
-export const SideMenuCategory = twc.div`group`
-export const SideMenuCategoryTitle = twc.div`px-2 py-1`
-export const SideMenuItem = twc.div`pl-4 leading-loose`
+
+export const SideMenuContainer = twc.div`bg-background text-muted-foreground mx-2 hidden flex-col gap-4 overflow-auto pb-8 text-sm break-words whitespace-normal md:flex`
+export const SideMenuCategory = twc.div`group font-medium`
+export const SideMenuCategoryTitle = twc.div`px-2 py-1 group-has-[.active]:bg-blue-50 group-has-[.active]:text-blue-700 rounded-md group-has-[.active]:font-bold`
+export const SideMenuContent = twc.div`grid gap-2 py-2`
+export const SideMenuSubContent = twc.div`grid gap-2 py-2`
+export const SideMenuItem = twc.div`pl-4`
 export const SideMenuNavLink = twc(NavLink).attrs({
   prefetch: 'intent',
-})`hover:underline aria-[current]:font-bold aria-[current]:text-foreground`
+})`hover:underline aria-[current]:text-blue-700`
